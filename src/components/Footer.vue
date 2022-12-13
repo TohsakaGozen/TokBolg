00e25a59ff92003fd0690a1cef0927c95d7aee6a
Tohsaka <1679124358@qq.com>
2022-12-13 13:41:52 +0800

    备案信息完善

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="footerBox">
-    <p>Copyright © 2022 TohsakaGozen All rights reserved</p>
-    <br />
-    <p>
-      TohsakaGozen个人所有
-      <a href="https://beian.miit.gov.cn"> 蜀ICP备2022028280号</a>
-    </p>
+    <div class="beianbox" style="width: 300px; margin: 0 auto; padding: 0">
+      <p>Copyright © 2022 TohsakaGozen All rights reserved</p>
+      <p>
+        <a href="https://beian.miit.gov.cn"> 蜀ICP备2022028280号</a>
+      </p>
+      <a
+        target="_blank"
+        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=51192302000224"
+        style="
+          display: inline-block;
+          text-align: center;
+          text-decoration: none;
+          height: 20px;
+          line-height: 20px;
+        "
+        ><img src="../assets/备案图标.png" style="float: left" />
+        <p
+          style="
+            float: left;
+            height: 20px;
+            line-height: 20px;
+            margin: 0px 0px 0px 5px;
+            color: #939393;
+          "
+        >
+          川公网安备 51192302000224号
+        </p></a
+      >
+    </div>
   </div>
 </template>
 
@@ -16,6 +39,11 @@ export default {
 </script>
 
 <style>
+.beianbox {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 .footerBox {
   position: absolute;
   align-self: flex-end;
